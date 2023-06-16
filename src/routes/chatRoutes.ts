8d4b447a569257ddde63556e3b7eedace433e92f
yash <yashsingh963080@gmail.com>
2023-06-16 18:27:14 +0530

    Removed editProfileImage middleware

--- src/routes/chatRoutes.ts
+++ src/routes/chatRoutes.ts
@@ -4,7 +4,7 @@ import chatControllers from '../controllers/chatControllers'
 import userAuthentication from '../middleware/userAuthentication'
 import cookieParser from 'cookie-parser'
 import handleFileUpload from '../middleware/handleFileUploads'
-import editProfileImage from '../middleware/editProfileImage'
+// import editProfileImage from '../middleware/EditProfileImage'
 chatRoutes.use(cookieParser())
 
 chatRoutes.put("/clear-chat",chatControllers.clearChat)
@@ -18,7 +18,7 @@ chatRoutes.put("/remove-admin",userAuthentication,chatControllers.removeAdmin)
 chatRoutes.put("/clear-chat",userAuthentication,chatControllers.clearChat)
 chatRoutes.put("/delete-chat",userAuthentication,chatControllers.deleteChat)
 chatRoutes.put("/leave-group",userAuthentication,chatControllers.leaveChat)
-chatRoutes.put("/update-info",userAuthentication,editProfileImage,chatControllers.updateProfileInfo)
+chatRoutes.put("/update-info",userAuthentication,chatControllers.updateProfileInfo)
 chatRoutes.put("/isChatExists",userAuthentication,chatControllers.getSingelChatWithUsers)
 
 export default chatRoutes
